337fc2ab2cc52f8ede938360a479fdd8f6603cc1
saranya <tsaranyasailakshmi@gmail.com>
2025-03-15 17:49:00 +0530

    templates,media,static folders created

--- ERP/qs/urls.py
+++ ERP/qs/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('index',views.vindex,name='nindex'),
 
 ]
+
--- ERP/qs/views.py
+++ ERP/qs/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-
 def vindex(request):
     return HttpResponse("Welcom to Index Page")
